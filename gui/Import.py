cb499cba6ec39fc5a095113662b49e995b48f411
Tabascl <simon@wildesw.com>
2018-12-12 19:28:41 +0100

    Add button and method stubs for arrive time

--- gui/Import.py
+++ gui/Import.py
@@ -8,17 +8,32 @@ class ImportDialog(wx.Dialog):
         self.table = CSVTable(filepath)
         self._init_controls()
         self._init_sizers()
+        self._init_events()
 
     def _init_controls(self):
         self.time_grid = wx.grid.Grid(self, -1)
         self.time_grid.SetTable(self.table)
         self.time_grid.AutoSize()
-    
+
+        self.arrive_btn = wx.Button(self, label='Set Arrive')
+
+    def _init_events(self):
+        self.Bind(wx.EVT_BUTTON, self._on_set_arrive, self.arrive_btn)
+
     def _init_sizers(self):
         main_sizer = wx.BoxSizer(wx.VERTICAL)
+        bottom_sizer = wx.BoxSizer(wx.HORIZONTAL)
         btn_sizer = self.CreateButtonSizer(wx.OK | wx.CANCEL)
 
+        bottom_sizer.Add(self.arrive_btn, 0)
+        bottom_sizer.AddStretchSpacer()
+        bottom_sizer.Add(btn_sizer, 0)
+
         main_sizer.Add(self.time_grid, 1, wx.EXPAND)
-        main_sizer.Add(btn_sizer, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5)
+        main_sizer.Add(bottom_sizer, 0, wx.EXPAND | wx.ALL, 5)
 
         self.SetSizerAndFit(main_sizer)
+
+    def _on_set_arrive(self, event):
+        topleft = self.time_grid.GetSelectionBlockTopLeft()
+        bottomright = self.time_grid.GetSelectionBlockTopRight()
