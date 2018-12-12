c0e307fe4c4ea60f7eb88b003abe8f6405f4e8da
Tabascl <simon@wildesw.com>
2018-12-12 19:09:59 +0100

    Convert read method to class function

--- data/CSVReader.py
+++ data/CSVReader.py
@@ -1,7 +1,7 @@
 import csv
 
 class CSVReader:
-    def read(self, path):
+    def read(path):
         with open(path, newline='', encoding='utf-8') as csvfile:
             rd = csv.reader(csvfile, delimiter=';')
             return [row for row in rd]
