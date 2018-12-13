ab6922eb4a2b7c3d3ba42c26e96b994c261d6d0c
Tabascl <simon@wildesw.com>
2018-12-13 12:11:23 +0100

    Add selection containers, reset colors on reselection

--- gui/Import.py
+++ gui/Import.py
@@ -10,6 +10,10 @@ class ImportDialog(wx.Dialog):
         self._init_sizers()
         self._init_events()
 
+        self.day_selection = None
+        self.arrive_selection = None
+        self.leave_selection = None
+
     def _init_controls(self):
         self.time_grid = wx.grid.Grid(self, -1)
         self.time_grid.SetTable(self.table)
@@ -46,15 +50,30 @@ class ImportDialog(wx.Dialog):
         self.SetSizerAndFit(main_sizer)
 
     def _on_set_day(self, event):
-        self._paint_selection('light blue')
+        if self.day_selection:
+            old_selection = self.day_selection
+        
+        self.day_selection = self._selection_to_coords()
+        self._paint_selection(old_selection, self.time_grid.GetDefaultCellBackgroundColour())
+        self._paint_selection(self.day_selection, 'light blue')
 
     def _on_set_arrive(self, event):
-        self._paint_selection(wx.RED)
+        if self.arrive_selection:
+            old_selection = self.day_selection
+        
+        self.arrive_selection = self._selection_to_coords()
+        self._paint_selection(old_selection, self.time_grid.GetDefaultCellBackgroundColour())
+        self._paint_selection(self.arrive_selection, wx.RED)
 
     def _on_set_leave(self, event):
-        self._paint_selection(wx.GREEN)
+        if self.leave_selection:
+            old_selection = self.day_selection
+        
+        self.leave_selection = self._selection_to_coords()
+        self._paint_selection(old_selection, self.time_grid.GetDefaultCellBackgroundColour())
+        self._paint_selection(self.leave_selection, wx.GREEN)
 
-    def _paint_selection(self, color):
+    def _selection_to_coords(self):
         try:
             topleft = self.time_grid.GetSelectionBlockTopLeft()[0]
             bottomright = self.time_grid.GetSelectionBlockBottomRight()[0]
@@ -68,7 +87,11 @@ class ImportDialog(wx.Dialog):
             elif rows:
                 topleft = [rows[0], 0]
                 bottomright = [rows[0], self.time_grid.NumberCols-1]
+        
+        return (topleft, bottomright)
 
+    def _paint_selection(self, selection, color):
+        topleft, bottomright = selection
         for x in range(topleft[0], bottomright[0]+1):
             for y in range(topleft[1], bottomright[1]+1):
                 self.time_grid.SetCellBackgroundColour(x, y, color)
