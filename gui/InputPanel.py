8be0b768583a8e2f11fb717fd79735f50fa01d22
Tabascl <simon@wildesw.com>
2018-12-13 19:06:05 +0100

    Add another TimePicker

--- gui/InputPanel.py
+++ gui/InputPanel.py
@@ -10,10 +10,8 @@ class InputPanel(wx.Panel):
         self._init_sizers()
 
     def _init_controls(self):
-        self.datepicker = wx.adv.DatePickerCtrl(self, style=wx.adv.DP_DROPDOWN)
-        self.datepicker.SetValue(wx.DateTime.Today())
-        
         self.timepicker_arrive = TimePicker(self)
+        self.timepicker_leave = TimePicker(self, type_arrive=False)
 
         self.company_cbox = wx.ComboBox(self, choices=["eso", "CREADIS"])
 
@@ -21,25 +19,33 @@ class InputPanel(wx.Panel):
 
     def _init_sizers(self):
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        main_sizer.Add(self.datepicker, 0, wx.ALL, 5)
         main_sizer.Add(self.timepicker_arrive, 0, wx.ALL, 5)
+        main_sizer.Add(self.timepicker_leave, 0, wx.ALL, 5)
         main_sizer.Add(self.company_cbox, 0, wx.ALL, 5)
         main_sizer.Add(self.task_description, 0, wx.ALL, 5)
         self.SetSizerAndFit(main_sizer)
 
 
 class TimePicker(wx.Panel):
-    def __init__(self, parent, initial=datetime.time(8)):
+    def __init__(self, parent, initial=datetime.time(8), type_arrive=True):
         wx.Panel.__init__(self, parent, style=wx.BORDER_NONE)
         self.time = initial
+        self.type_arrive = type_arrive
         self._build()
 
     def _build(self):
-        self.hour_field = wx.TextCtrl(self, value=self.time.strftime('%H'), size=(30, -1))
-        self.minute_field = wx.TextCtrl(self, value=self.time.strftime('%M'), size=(30, -1))
+        if self.type_arrive:
+            path = 'img/arrow_arrive.png'
+        else:
+            path = 'img/arrow_leave.png'
+        bmp = wx.Bitmap(path, wx.BITMAP_TYPE_PNG)
+        self.arrow = wx.StaticBitmap(self, bitmap=bmp, size=(-1, 20))
+        self.hour_field = wx.TextCtrl(self, value=self.time.strftime('%H'), size=(25, -1))
+        self.minute_field = wx.TextCtrl(self, value=self.time.strftime('%M'), size=(25, -1))
         self.separator = wx.StaticText(self, label=':')
 
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        main_sizer.Add(self.arrow, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, 3)
         main_sizer.Add(self.hour_field, 0)
         main_sizer.Add(self.separator, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 3)
         main_sizer.Add(self.minute_field, 0)
